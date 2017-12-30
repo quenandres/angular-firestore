9a9b9b0448a5d3d215041cac6e377d4826f6af26
quenandres <quenandres@gmail.com>
2017-12-30 14:16:10 -0500

    entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
